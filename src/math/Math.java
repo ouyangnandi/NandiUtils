c4a87b5d167fa1293b177beead86643ffb93fc2a
Nandi Ouyang <ouyangnandi@hotmail.com>
2015-03-30 22:38:39 +0800

    Change the text

--- src/math/Math.java
+++ src/math/Math.java
@@ -12,7 +12,7 @@ public class Math {
 	 * first part: find all bit value =1 in val1 and val2,using "And" operation
 	 * second part: using XOR to merge all bit value 0 and bit value 1 in val1 and val2, then divided by two
 	 */
-	public static int towIntAvg(int val1,int val2){
+	public static int twoIntAvg(int val1,int val2){
 		
 		return (val1&val2) + ((val1^val2)>>1);
 		
