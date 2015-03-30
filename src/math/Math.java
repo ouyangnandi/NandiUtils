0de1e021789ca2967613ec2cb95e44d8c3ec3f2d
Nandi Ouyang <ouyangnandi@hotmail.com>
2015-03-30 22:32:28 +0800

    create project

---
+++ src/math/Math.java
@@ -0,0 +1,21 @@
+package math;
+
+/**
+ * @Author: Nandi Ouyang
+ * Mar 30, 2015
+ *
+ */
+public class Math {
+	
+	/**
+	 * Get average integer number between val1 and val2. Prevent integer number overflow 
+	 * first part: find all bit value =1 in val1 and val2,using "And" operation
+	 * second part: using XOR to merge all bit value 0 and bit value 1 in val1 and val2, then divided by two
+	 */
+	public static int towIntAvg(int val1,int val2){
+		
+		return (val1&val2) + ((val1^val2)>>1);
+		
+	}
+
+}
